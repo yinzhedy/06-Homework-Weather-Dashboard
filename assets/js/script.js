74d50059d82d4f3a5de6217c9ead36e127d07bf2
yinzhedy <yinzhe.dy@gmail.com>
2021-09-22 00:24:12 -0400

    added set wind data to local storage under click event listener for search button, and created function to display wind speed in today's weather column. Issue with no value being found in storage on first click, but is working correctly on second click. Will troubleshoot.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var cityName = document.getElementById("city-name-input");
 var searchButton = document.getElementById("search-button");
 const apiKey = "8e6bbf77ae628080f9111805bc8624fb";
+var todayTempDisplay = document.getElementById("today-temp");
+var todayWindDisplay = document.getElementById("today-wind");
+var todayHumidityDisplay = document.getElementById("today-humidity");
+var todayUvDisplay = document.getElementById("today-uv-index");
 
 searchButton.addEventListener("click", function() {
     var city = cityName.value;
@@ -10,7 +14,16 @@ searchButton.addEventListener("click", function() {
     .then(function(response) {
         response.json()
         .then(function(data){
-            console.log(data)
+            var wind = "Wind Speed:" + data.wind.speed + " MPH";
+            console.log(data);
+            localStorage.setItem("data", wind);
         });
     })
+    displayTodaysWeather();
 })
+
+function displayTodaysWeather() {
+    var wind = localStorage.getItem("data", wind);
+    console.log(wind);
+    todayWindDisplay.textContent = wind;
+}
