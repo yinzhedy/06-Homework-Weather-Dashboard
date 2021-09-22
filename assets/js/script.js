5bdba63f88a6ae19fc0d99ea703ee2c100f4d0aa
yinzhedy <yinzhe.dy@gmail.com>
2021-09-22 01:19:29 -0400

    linked in moment in html. using fetched data and moment, got current day time and location to show via the todays weather section. got temp to appear in todays weather function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var cityName = document.getElementById("city-name-input");
 var searchButton = document.getElementById("search-button");
 const apiKey = "8e6bbf77ae628080f9111805bc8624fb";
+var todayLocationAndDateDisplay = document.getElementById("location-and-date");
 var todayTempDisplay = document.getElementById("today-temp");
 var todayWindDisplay = document.getElementById("today-wind");
 var todayHumidityDisplay = document.getElementById("today-humidity");
 var todayUvDisplay = document.getElementById("today-uv-index");
+var momentTodaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
 searchButton.addEventListener("click", function() {
     var city = cityName.value;
@@ -18,14 +20,18 @@ searchButton.addEventListener("click", function() {
             var wind = "Wind Speed:" + data.wind.speed + " MPH";
             var humidity = "Humidity:" + data.main.humidity + "%";
             var uvIndex = "UV Index:" + data.main.uvi;
-            console.log(temp);
+            var locationAndDate = " City : " + city + " Date & Time: " + momentTodaysDate;
             console.log(data);
+            console.log(temp);
+            console.log(wind);
             console.log(humidity);
             console.log(uvIndex);
+            console.log(locationAndDate)
             localStorage.setItem("dataTemp", temp);
             localStorage.setItem("dataWindSpeed", wind);
             localStorage.setItem("dataHumidity", humidity);
             localStorage.setItem("dataUvIndex", uvIndex);
+            localStorage.setItem("locationAndDate", locationAndDate);
         });
     })
     displayTodaysWeather();
@@ -33,7 +39,20 @@ searchButton.addEventListener("click", function() {
 })
 
 function displayTodaysWeather() {
-    var wind = localStorage.getItem("data", wind);
+    var temp = localStorage.getItem("dataTemp", temp);
+    var wind = localStorage.getItem("dataWindSpeed", wind);
+    var humidity = localStorage.getItem("dataHumidity", humidity);
+    var uvIndex = localStorage.getItem("dataUvIndex", uvIndex);
+    var locationAndDate = localStorage.getItem("locationAndDate", locationAndDate);
+    console.log(temp);
     console.log(wind);
+    console.log(humidity);
+    console.log(uvIndex);
+    todayLocationAndDateDisplay.textContent = locationAndDate;
+    todayTempDisplay.textContent = temp;
     todayWindDisplay.textContent = wind;
+    todayHumidityDisplay.textContent = humidity;
+    todayUvDisplay.textContent = uvIndex;
+    return;
 }
+
